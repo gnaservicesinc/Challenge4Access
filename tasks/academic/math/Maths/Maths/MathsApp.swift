a443055afb1f045d6e428bc9357ccfb28bb3b8ef
gnaservicesinc <alast.andy@gmail.com>
2025-08-16 10:04:37 -0400

    housekeeping

--- tasks/academic/math/Maths/Maths/MathsApp.swift
+++ tasks/academic/math/Maths/Maths/MathsApp.swift
@@ -5,6 +5,7 @@
 //  Created by Andrew Smith on 8/16/25.
 //
 let C4A_OUR_SETTINGS_FILE = "/opt/c4a/protected/ro/task_settings/maths"
+let C4A_OUR_MEMORY_FILE = "/opt/c4a/protected/memory/app_mem/maths"
 
 import SwiftUI
 
